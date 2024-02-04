228a9538349ff95c528ab15082ae53e5e0322fc7
AyeChan Han <ayechanhan@AyeChans-MacBook.local>
2024-02-05 03:15:55 +0800

    Add spacer component and optimize it

---
+++ src/components/spacer/spacer.component.js
@@ -0,0 +1,29 @@
+import styled from "styled-components/native";
+
+const sizeVariant = {
+  small: 1,
+  medium: 2,
+  large: 3,
+};
+
+const positionVariant = {
+  top: "margin-top",
+  left: "margin-left",
+  bottom: "margin-bottom",
+  right: "margin-right",
+};
+
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
+  return `${property}: ${value}`;
+};
+
+export const Spacer = styled.View`
+  ${({ position, size, theme }) => getVariant(position, size, theme)}
+`;
+Spacer.defaultProps = {
+  position: "top",
+  size: "small",
+};
--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
 import styled from "styled-components/native";
 import { Card, Text } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
+import { Spacer } from "../../../components/spacer/spacer.component";
 
 const RestaurantCard = styled(Card)`
   background: ${(props) => props.theme.colors.bg.primary};
@@ -31,21 +32,28 @@ const Rating = styled.View`
   padding-bottom: ${(props) => props.theme.space[2]};
 `;
 
-const OpenNow = styled.View`
+const Section = styled.View`
   flex-direction: row;
+  align-items: center;
+`;
+
+const OpenNow = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-end;
 `;
 
 export const RestaurntInfo = ({ restaurant = {} }) => {
   const {
     name = "Song Fa Buk Kut Teh",
-    icon,
+    icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
     images = [
       "https://songfa.com.sg/cdn/shop/files/SFBKT_BibGoumand23_WebBanner_PC_365968b7-046e-4063-bd27-65888c334717_1439x400@2x.jpg?v=1686810779",
     ],
     address = "238 Thomson Road Singapore 307683",
     isOpenNow = true,
     rating = 5,
-    isClosedTemporarily = false,
+    isClosedTemporarily = true,
   } = restaurant;
 
   const ratingArray = Array.from(new Array(Math.floor(rating)));
@@ -59,14 +67,29 @@ export const RestaurntInfo = ({ restaurant = {} }) => {
       />
       <Card.Content>
         <RestaurantCardTitle variant="titleLarge">{name}</RestaurantCardTitle>
-        <Rating>
-          {ratingArray.map((data, idx) => (
-            <SvgXml xml={star} width={20} height={20} key={idx} />
-          ))}
-        </Rating>
-        <OpenNow>
-          <SvgXml xml={open} width={20} height={20} />
-        </OpenNow>
+        <Section>
+          <Rating>
+            {ratingArray.map((data, idx) => (
+              <SvgXml xml={star} width={20} height={20} key={idx} />
+            ))}
+            <OpenNow>
+              {isClosedTemporarily && (
+                <Text variant="labelLarge" style={{ color: "red" }}>
+                  CLOSED TEMPORARILY
+                </Text>
+              )}
+              <Spacer position="left" size="large">
+                {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+              </Spacer>
+              <Spacer position="left" size="large">
+                <Image
+                  style={{ width: 15, height: 15 }}
+                  source={{ uri: icon }}
+                />
+              </Spacer>
+            </OpenNow>
+          </Rating>
+        </Section>
         <RestaurantCardAddress variant="bodyMedium">
           {address}
         </RestaurantCardAddress>
